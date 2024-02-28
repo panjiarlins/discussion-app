71e6e381ac87c915dc3ef54481300940dbf6dcd1
Panji Arlin Saputra <75957797+panjiarlins@users.noreply.github.com>
2024-02-28 14:01:35 +0700

    test: add tests for getThreads thunk

--- src/store/threadsSlice.thunk.test.ts
+++ src/store/threadsSlice.thunk.test.ts
@@ -1,10 +1,11 @@
 import { setupStore } from './store'
-import { getAllThreads } from './threadsSlice'
+import { getAllThreads, getThreads } from './threadsSlice'
 import { hideLoading, showLoading } from 'react-redux-loading-bar'
 import api from '@/lib/api'
 import users from '@/__tests__/test-data/users'
 import threads from '@/__tests__/test-data/threads'
 import allThreads from '@/__tests__/test-data/allThreads'
+import filterThreads from '@/utils/filter-threads'
 
 // Mocking API
 jest.mock('../lib/api', () => ({
@@ -69,5 +70,61 @@ describe('getAllThreads thunk', () => {
     expect(dispatchSpy).toHaveBeenCalledWith(hideLoading(actionType))
     const result = dispatchSpy.mock.results.slice(-1)[0].value
     expect(result.type).toBe(getAllThreads.rejected.type)
+  })
+})
+
+/**
+ * getThreads thunk
+ * - should create action type correctly
+ * - should dispatch action correctly when data fetching success
+ * - should dispatch action correctly when data fetching failed
+ */
+describe('getThreads thunk', () => {
+  const actionType = 'threads/getThreads'
+
+  it('should create action type correctly', () => {
+    const { typePrefix, fulfilled, pending, rejected } = getThreads
+    expect(typePrefix).toBe(actionType)
+    expect(fulfilled.type).toBe(`${actionType}/fulfilled`)
+    expect(pending.type).toBe(`${actionType}/pending`)
+    expect(rejected.type).toBe(`${actionType}/rejected`)
+  })
+
+  it('should dispatch action correctly when data fetching success', async () => {
+    const store = setupStore()
+    const dispatchSpy = jest.spyOn(store, 'dispatch')
+    const apiSpy = jest.spyOn(api, 'get')
+    const searchParams = new URLSearchParams()
+
+    const thunkFunction = getThreads({ searchParams })
+    await thunkFunction(store.dispatch, () => store.getState(), undefined)
+
+    expect(dispatchSpy).toHaveBeenCalledWith(showLoading(actionType))
+    expect(dispatchSpy).toHaveBeenCalledWith(expect.any(Function))
+    expect(apiSpy).toHaveBeenCalledTimes(2)
+    expect(apiSpy).toHaveBeenCalledWith('/users')
+    expect(apiSpy).toHaveBeenCalledWith('/threads')
+    expect(dispatchSpy).toHaveBeenCalledWith(hideLoading(actionType))
+    const result = dispatchSpy.mock.results.slice(-1)[0].value
+    expect(result.type).toBe(getThreads.fulfilled.type)
+    expect(result.payload).toEqual(filterThreads(allThreads, searchParams))
+  })
+
+  it('should dispatch action correctly when data fetching failed', async () => {
+    const store = setupStore()
+    const dispatchSpy = jest.spyOn(store, 'dispatch')
+    jest.spyOn(api, 'get').mockRejectedValueOnce(new Error('error'))
+    const searchParams = new URLSearchParams()
+
+    const thunkFunction = getThreads({ searchParams })
+    await thunkFunction(store.dispatch, () => store.getState(), undefined)
+
+    expect(dispatchSpy).toHaveBeenCalledWith(showLoading(actionType))
+    expect(dispatchSpy).toHaveBeenCalledWith(expect.any(Function))
+    expect(dispatchSpy).toHaveBeenCalledWith(hideLoading(actionType))
+    const result = dispatchSpy.mock.results.slice(-1)[0].value
+    expect(result.payload).toEqual(
+      filterThreads(store.getState().threads.allThreads, searchParams)
+    )
   })
 })
