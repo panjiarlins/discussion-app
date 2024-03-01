d40ef1db86ddef7969d395cbc4c12d7a31030071
Panji Arlin Saputra <75957797+panjiarlins@users.noreply.github.com>
2024-03-01 17:25:40 +0700

    test: CommentButton component

--- src/components/home/comment-button.tsx
+++ src/components/home/comment-button.tsx
@@ -13,12 +13,12 @@ export default function CommentButton({
   return (
     <Button
       asChild
+      role="button"
       variant="ghost"
       className="w-24 space-x-1 rounded-full"
-      type="button"
     >
       <Link href={`/threads/${threadId}#comments`}>
-        <MessageCircleMore className="size-6 stroke-primary" />
+        <MessageCircleMore role="img" className="size-6 stroke-primary" />
         <span>{totalComments}</span>
       </Link>
     </Button>
