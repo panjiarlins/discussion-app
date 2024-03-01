44db99b091b64ed99e37024e9b5a302ff225771c
Panji Arlin Saputra <75957797+panjiarlins@users.noreply.github.com>
2024-03-01 23:57:43 +0700

    test: home/@sidebar/LogoutButton component

--- src/components/home/@sidebar/logout-button.tsx
+++ src/components/home/@sidebar/logout-button.tsx
@@ -6,9 +6,11 @@ import signOut from '@/lib/sign-out'
 export default function LogoutButton() {
   return (
     <Button
+      aria-label="Log out"
       variant="ghost"
       className="rounded-none font-bold"
       onClick={signOut}
+      type="button"
     >
       Log out
     </Button>
